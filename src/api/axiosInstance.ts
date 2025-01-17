40bd139cca20f65819ba0f84e866a6193b51ab64
Gidiz <gidizimmer@gmail.com>
2025-01-17 09:24:26 +0200

    feat: fetching findings from the BE

---
+++ src/api/axiosInstance.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+//todo: use environemnt var for baseUrl
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:4000",
+});
+
+//todo important: store cookies more securely
+
+axiosInstance.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem("accessToken");
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
+
+export default axiosInstance;
