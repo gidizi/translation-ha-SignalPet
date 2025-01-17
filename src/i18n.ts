fa33f6c4484654f73c2357583efb827e73033391
Gidiz <gidizimmer@gmail.com>
2025-01-17 14:11:47 +0200

    feat: bonus point 4

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,15 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
+//note: it worth try to find some method to sync between i18n supportedlngs lists and the libre configuration
 i18next
   .use(initReactI18next)
+  .use(LanguageDetector)
   .use(Backend)
   .init({
+    supportedLngs: ["en", "es", "de", "pt", "fr"],
     fallbackLng: "en",
     backend: {
       loadPath: "/locales/i18n/{{lng}}/{{ns}}.json",
